4d3cbecdb31a371573f4c73c92941d9459970636
IgorShumsky <Tracho2013@gmail.com>
2026-05-26 04:56:13 +0300

    ы

--- js/modules/components/getSorage.js
+++ js/modules/components/getSorage.js
@@ -1,11 +1,26 @@
 function getStorage(mainQuestions_length, nameStorage) {
-  // console.log(nameStorage,mainQuestions_length)
-  return (JSON.parse(localStorage.getItem(nameStorage)) || {
+  // 1. Пытаемся достать данные из localStorage
+  const localData = JSON.parse(localStorage.getItem(nameStorage));
+
+  // 2. Создаем структуру по умолчанию, которая гарантированно содержит ВСЕ ключи
+  const defaultStructure = {
     correct: [],
     incorrect: [],
     neverSeen: Array.from({ length: mainQuestions_length }, (_, i) => i),
-    saveList: [],
-  });
+    saveList: []
+  };
+
+  // 3. Если в localStorage вообще ничего нет, возвращаем структуру по умолчанию
+  if (!localData) {
+    return defaultStructure;
+  }
+
+  // 4. УМНОЕ ОБЪЕДИНЕНИЕ: Берем структуру по умолчанию и поверх накладываем localData.
+  // Если какого-то ключа (например, saveList) в localData не было, он возьмется из defaultStructure.
+  // Все старые данные (correct, incorrect и т.д.) останутся нетронутыми.
+  const mergedStorage = { ...defaultStructure, ...localData };
+
+  return mergedStorage;
 }
 
 export default getStorage;
